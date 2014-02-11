228a2a169832a6bf37440084bc64d2e763a5fca8
eric@urbanrage.com <eric@urbanrage.com>
2014-02-11 17:01:51 -0600

    fixed knock.py, should work now

--- knock.py
+++ knock.py
@@ -13,21 +13,25 @@ import hashlib
 import binascii
 import random
 import socket
-from struct import *
-from random import *
+import argparse
+import struct
 from ConfigParser import ConfigParser
-from optparse import OptionParser
 
 class Knock():
-    def __init__(host, key, tag='PKD0', old=None):
+    def __init__(self, host, key, tag='PKD0', old=None):
         """Initialize with host = hostname or ip, key = hex string or byte string to use as the key, tag = 4 byte id, old if use old style for creating knock packet"""
         self.host = host
         if key.startswith("0x"):
             self.key = binascii.a2b_hex(key[2:])
         else:
             self.key = key
+        l = len(self.key)
+        for i in range(0, 40-l):
+            self.key += '\0'
+        self.key = self.key[:40]
+
         if len(tag) != 4:
-            if not tag.startswith('0x')
+            if not tag.startswith('0x'):
                 raise ValueError("Invalid tag")
             else:
                 self.tag = binascii.a2b_hex(tag[2:])
@@ -43,25 +47,21 @@ class Knock():
             rport = 0
         if rport <= 0 or rport > 65535:
             rport = random.randint(1024, 50000)
-        sock = socket.socket(AF_INET, SOCK_DGRAM)
-        sock.bind(('', port))
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind(('', rport))
 
-        l = len(self.key)
-        for i in range(0, 40-l):
-            self.key += '\0'
-
-        bport = pack("<BBBB", ((rport & 0xff00) >> 8), rport & 0xff, (rport & 0xff00) >> 8, rport & 0xff)
-        p = self.tag + pack("<IIIII", int(time.time()), 0, os.urandom(4), os.urandom(4), os.urandom(4))
+        bport = struct.pack("<BBBB", ((rport & 0xff00) >> 8), rport & 0xff, (rport & 0xff00) >> 8, rport & 0xff)
+        p = self.tag + struct.pack("<II", int(time.time()), 0) + os.urandom(12)
         if self.__use_old:
-            ssum = p + bkey
+            ssum = p + self.key
         else:
-            ssum = bport + p + bkey
+            ssum = bport + p + self.key
         m = hashlib.sha256()
         m.update(ssum);
         d = m.digest()
 
         packet = p + d;
-        sock.sendto(packet, (self.host, self.port))
+        sock.sendto(packet, (self.host, rport))
 
 def __main():
     """provide a default main program for the module"""
@@ -72,21 +72,20 @@ def __main():
             help='key to use for this knock, overrides config entry if there is one')
     parser.add_argument('-t', '--tag',
             help='tag to use for this knock, overrides config entry if there is one')
-    parser.add_argument('-p', '--port', type=int
+    parser.add_argument('-p', '--port', type=int,
             help='port to use for this knock, overrides config entry if there is one')
     parser.add_argument('-q', '--quiet', action='store_true',
             help='be quiet about the operation')
-    parser.add_argument('host', nargs'+',
-            help='host to knock, or section name in config file'
+    parser.add_argument('host', nargs='+',
+            help='host to knock, or section name in config file')
 
     args = parser.parse_args()
 
     config = ConfigParser()
-    read = config.read(options.configfile)
+    read = config.read(args.config)
     if (len(read) == 0):
-        if not options.quiet: print "no config files found, default config file is ~/.ipt_pkd.ini\n"
-        exit(0)
-
+        if args.key is None:
+            parser.error("You must either have a config file or specify a key")
 
     for site in (args.host):
         if (config.has_section(site)):
@@ -122,7 +121,7 @@ def __main():
             k = Knock(host, key, tag, old)
             k.knock(port)
 
-            if not options.quiet: print "Sent knock packet to", host
+            if not args.quiet: print "Sent knock packet to", host
         else:
             if args.key is not None:
                 key = args.key
@@ -130,9 +129,9 @@ def __main():
                 tag = args.tag
                 k = Knock(host, key, tag, False)
                 k.knock(port)
-                if not options.quiet: print "Sent knock packet to", host
+                if not args.quiet: print "Sent knock packet to", host
             else:
-                if not options.quiet: print "Section [", site, "] doesn't exist in", options.configfile
+                if not args.quiet: print "Section [", site, "] doesn't exist in", options.configfile
 
 
 if __name__ == "__main__":
